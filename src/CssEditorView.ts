3635a3d08d27890026574270e3723848a87e8468
Zachatoo <Zachatoo@users.noreply.github.com>
2023-05-26 21:51:17 -0600

    Focus editor on open css editor view

--- src/CssEditorView.ts
+++ src/CssEditorView.ts
@@ -45,6 +45,11 @@ export class CssEditorView extends ItemView {
 			this.dispatchEditorData(data);
 			this.app.workspace.requestSaveLayout();
 		}
+		const timer = window.setInterval(() => {
+			this.editor.focus();
+			if (this.editor.hasFocus) clearInterval(timer);
+		}, 200);
+		this.registerInterval(timer);
 	}
 
 	getEditorData() {
