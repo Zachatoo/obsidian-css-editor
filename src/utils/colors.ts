8672faf97700edc60842f56864bc00bc959f0907
Zachatoo <Zachatoo@users.noreply.github.com>
2025-08-21 23:24:26 -0600

    Show color picker for named colors
    refs: #43

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -17,7 +17,6 @@ const hexColorRegexes = [
 	`#(?:[0-9a-fA-F]{3}){1,2}`,
 	`#(?:[0-9a-fA-F]{4}){1,2}`,
 ];
-const hexColorRegex = new RegExp(`(${hexColorRegexes.join("|")})`, "g");
 const rgbColorRegexes = [
 	// Legacy rgb/rgba: rgba(1,2,3,0.5)
 	String.raw`rgba?\(\s*${num}\s*,\s*${num}\s*,\s*${num}\s*(?:,\s*${alpha}\s*)?\)`,
@@ -31,10 +30,19 @@ const hslColorRegexes = [
 	// Modern hsl/hsla/hwb: hsla(1deg 2% 3%/0.5)
 	String.raw`h(?:sla?|wb)\(\s*${hue}\s+${numOrPercent}\s+${numOrPercent}\s*(?:/\s*${alpha}\s*)?\)`,
 ];
+const namedColorRegex = `aliceblue|antiquewhite|aqua|aquamarine|azure|beige|bisque|black|blanchedalmond|blue|blueviolet|brown|burlywood|cadetblue|chartreuse|chocolate|coral|cornflowerblue|cornsilk|crimson|cyan|darkblue|darkcyan|darkgoldenrod|darkgray|darkgreen|darkgrey|darkkhaki|darkmagenta|darkolivegreen|darkorange|darkorchid|darkred|darksalmon|darkseagreen|darkslateblue|darkslategray|darkslategrey|darkturquoise|darkviolet|deeppink|deepskyblue|dimgray|dimgrey|dodgerblue|firebrick|floralwhite|forestgreen|fuchsia|gainsboro|ghostwhite|gold|goldenrod|gray|green|greenyellow|grey|honeydew|hotpink|indianred|indigo|ivory|khaki|lavender|lavenderblush|lawngreen|lemonchiffon|lightblue|lightcoral|lightcyan|lightgoldenrodyellow|lightgray|lightgreen|lightgrey|lightpink|lightsalmon|lightseagreen|lightskyblue|lightslategray|lightslategrey|lightsteelblue|lightyellow|lime|limegreen|linen|magenta|maroon|mediumaquamarine|mediumblue|mediumorchid|mediumpurple|mediumseagreen|mediumslateblue|mediumspringgreen|mediumturquoise|mediumvioletred|midnightblue|mintcream|mistyrose|moccasin|navajowhite|navy|oldlace|olive|olivedrab|orange|orangered|orchid|palegoldenrod|palegreen|paleturquoise|palevioletred|papayawhip|peachpuff|peru|pink|plum|powderblue|purple|rebeccapurple|red|rosybrown|royalblue|saddlebrown|salmon|sandybrown|seagreen|seashell|sienna|silver|skyblue|slateblue|slategray|slategrey|snow|springgreen|steelblue|tan|teal|thistle|tomato|turquoise|violet|wheat|white|whitesmoke|yellow|yellowgreen`;
+
+const hexOrNamedColorRegexes = [...hexColorRegexes, namedColorRegex];
+const hexOrNamedColorRegex = new RegExp(
+	`(${hexOrNamedColorRegexes.join("|")})`,
+	"g"
+);
+
 const colorRegexes = [
 	...hexColorRegexes,
 	...rgbColorRegexes,
 	...hslColorRegexes,
+	namedColorRegex,
 ];
 const colorRegex = new RegExp(`(${colorRegexes.join("|")})`, "g");
 
@@ -69,8 +77,8 @@ export function convertToColorModel(color: string, model: string): string {
 
 export function getColorModel(color: string): string {
 	try {
-		hexColorRegex.lastIndex = 0;
-		if (hexColorRegex.test(color)) {
+		hexOrNamedColorRegex.lastIndex = 0;
+		if (hexOrNamedColorRegex.test(color)) {
 			return "hex";
 		}
 		return Color(color).toJSON().model;
